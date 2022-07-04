f15497a28c83a93c254f7732821439c624c0b99c
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-07-05 01:21:53 +0900

    update example

--- linebot_project/linebot/linebot/local_settings.example.py
+++ linebot_project/linebot/linebot/local_settings.example.py
@@ -1,8 +1,8 @@
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-07br8hi(@^=df_jdtf#&3eh8u2#u_0o&$m=p)2$y9g8u6zu4($'
+SECRET_KEY = ''
 
 DB_NAME='linebot_db'
 DB_USER = 'postgres'
 DB_PASSWORD = 'password'
 DB_HOST = '127.0.0.1'
-DB_PORT = '5433'
+DB_PORT = '5432'
