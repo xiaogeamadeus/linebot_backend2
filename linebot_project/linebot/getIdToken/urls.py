b060759bf17b6126ae22db1c295486558f58b4e6
XiaoGeAmadeus <xiaogeamadeus@gmail.com>
2022-07-19 16:57:14 +0900

    add API activate

--- linebot_project/linebot/getIdToken/urls.py
+++ linebot_project/linebot/getIdToken/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('login', views.index, name='index'),
     path('bot', views.root, name='root'),
     path('bot/<str:id>', views.id, name='show'),
+    path('bot/activate', views.activate),
 ]
