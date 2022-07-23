2832dc7e34cbe0961fe96c3d9503593778a41286
wang <xiaogeamadeus@gmail.com>
2022-07-23 14:41:10 +0900

    add an allowed_host 'kulinebotmaker.herokuapp.com' in settings.py

--- linebot_project/linebot/linebot/settings.py
+++ linebot_project/linebot/linebot/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','.ngrok.io']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','.ngrok.io','kulinebotmaker.herokuapp.com']
 # add cors
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
