21ffab75b491178aaddbac4f42ec00c3d5a8b899
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-09-16 23:37:13 +0900

    add question block

--- linebot/getIdToken/urls.py
+++ linebot/getIdToken/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('login', views.login, name='index'),
-    path('bot/others', views.others_bot, name='others_bot'),
-    path('bot', views.root, name='root'),
-    path('activate', views.activate, name='activate'),
-    path('bot/<str:id>', views.id, name='show'),
+    path("login", views.login, name="index"),
+    path("bot/others", views.others_bot, name="others_bot"),
+    path("bot/available", views.available_bot, name="available_bot"),
+    path("bot", views.root, name="root"),
+    path("activate", views.activate, name="activate"),
+    path("bot/<str:id>", views.id, name="show"),
 ]
